9501de82728a647eac8225db4de24d7c7114932f
PRishabhMishra <prishabh1999@gmail.com>
2024-01-04 18:59:13 -0800

    Second

--- src/main/java/com/driver/model/User.java
+++ src/main/java/com/driver/model/User.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name = "user")
+@Table(name = "`user`")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
